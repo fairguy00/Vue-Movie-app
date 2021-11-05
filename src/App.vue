cf8965a0ff7024d92822436c907c56688efd0481
fairguy00 <thbusae@gmail.com>
2021-11-05 17:04:04 +0900

    기본 컴포넌트 생성

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,24 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<!-- 뷰티파이기능이 전역적으로 사용가능하므로 script 에 임포트 안하고 사용가능하다 
+v-app 가 최상위 컴포넌트 그러므로 v-app 안에만 태그가 유효함-->
+  <v-app>
+    <v-container>
+      <!-- 검색바 -->
+      <search-bar/>
+      <!-- 영화목록 -->
+      <movie-list/>
+    </v-container>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SearchBar from '@/components/SearchBar'
+import MovieList from '@/components/MovieList'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+export default{
+  components:{
+    SearchBar,
+    MovieList
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <!-- vuetify의 input tag -->
+        <v-text-field
+            v-model="title">
+
+        </v-text-field>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            title:''
+        }
+    }
+}
+</script>
