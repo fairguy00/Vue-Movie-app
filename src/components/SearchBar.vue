1a06a1bc99c1755be24aeb259bccbfc9053eb6e5
fairguy00 <thbusae@gmail.com>
2021-11-06 16:20:54 +0900

    searchBar 작업
    아이콘 생성 및 console.log eslint허용

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,18 +1,36 @@
 <template>
-    <div>
-        <!-- vuetify의 input tag -->
-        <v-text-field
-            v-model="title">
-
-        </v-text-field>
-    </div>
+  <div>
+    <!-- vuetify의 input tag -->
+    <v-text-field 
+        v-model="title" 
+        outlined
+        @keypress.enter="searchMovies">
+        <template v-slot:prepend-inner>
+            <v-icon>search</v-icon>
+        </template>
+        <template v-slot:append>
+            <v-progress-circular
+                v-if="loading"
+                size="24"
+                color="primary"
+                indeterminate
+            />
+        </template>    
+    </v-text-field>
+  </div>
 </template>
 <script>
 export default {
-    data(){
-        return{
-            title:''
-        }
-    }
-}
+  data() {
+    return {
+      title: "",
+      loading:false
+    };
+  },
+  methods:{
+      searchMovies (){
+          console.log("searchMovies")
+      }
+  }
+};
 </script>
