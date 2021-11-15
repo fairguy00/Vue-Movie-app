011f8958e6cf30ecd6e644807b3bddf1664fcd48
fairguy00 <thbusae@gmail.com>
2021-11-15 23:57:32 +0900

    new Promise(async 문제 처리
    eslint 버전 5로 낮춤

--- src/store/movie.js
+++ src/store/movie.js
@@ -25,14 +25,10 @@ export default{
     },
     actions: {//비동기처리가능, state의 값을 갱신하려면 mutations
         fetchMovies({state, commit}, pageNum) {
-            const promise = new Promise(resolve => {
-                const res = axios.get(`http://www.omdbapi.com/?apikey=a08e2e9&s=${state.title}&page=${pageNum}`)
-                resolve(res)//리졸브에 인수로 데이터를 넣으면 밖에서 반환받아 사용가능
-            })
-            return promise.then(res => {
-                res.data
-                commit('pushIntoMovies',res.data.Search)
-                //console.log("res.data: "+res.data)
+            return new Promise(async resolve => {
+                const res = await axios.get(`http://www.omdbapi.com/?apikey=a08e2e9&s=${state.title}&page=${pageNum}`)
+                commit('pushIntoMovies', res.data.Search)
+                resolve(res.data)//리졸브에 인수로 데이터를 넣으면 밖에서 반환받아 사용가능
             })
         },
         async searchMovies ({commit,dispatch}){//actions 사용할땐 dispatch
@@ -41,7 +37,7 @@ export default{
                 loading: true,
                 movies:[]
             })
-            const { totalResults } = await dispatch('fetchMovies',1)
+            const { totalResults } = await dispatch('fetchMovies', 1)
             //const res = await axios.get(`http://www.omdbapi.com/?apikey=a08e2e9&s=${state.title}&page=1`)//promise객체반환 - 비동기
             const pageLength = Math.ceil(totalResults / 10)
 
